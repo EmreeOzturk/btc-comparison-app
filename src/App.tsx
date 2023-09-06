d092db5eef0b3b8c1846c5a769eb68c9566513af
Emre Ozturk <emreeozturk42@gmail.com>
2023-09-06 04:45:57 +0300

    linked backend to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import AmountInput from "./components/AmountInput"
 import ResultRow from "./components/ResultRow"
-
+import axios from 'axios'
 function App() {
   const [amount, setAmount] = useState('')
+  const [cached, setCached] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    axios.get('https://hjidbztxxw.us.aircode.run/cachedValues').then((res) => {
+      console.log(res.data)
+      setCached(res.data)
+      setLoading(false)
+    }
+    )
+  }, [])
   return (
     <main className=" max-w-3xl mx-auto p-8">
       <h1 className="uppercase text-6xl text-center font-bold
         bg-gradient-to-br from-indigo-400 to-purple-800 bg-clip-text text-transparent from-20%
       ">Find <b>cheapest</b> BTC </h1>
-      <div className="flex justify-center mt-10">
+      <div className="flex justify-center my-6">
         <AmountInput
           value={amount}
           onChange={
@@ -17,11 +28,35 @@ function App() {
           }
         />
       </div>
-      <div className="mt-10">
-        <ResultRow />
-        <ResultRow />
-        <ResultRow />
-        <ResultRow />
+      {
+        loading && (
+          <div>
+            <ResultRow loading={loading} />
+            <ResultRow loading={loading} />
+            <ResultRow loading={loading} />
+            <ResultRow loading={loading} />
+            <ResultRow loading={loading} />
+          </div>
+        )
+      }
+      <div>
+        {
+          cached.map((item: {
+            id: string,
+            provider: string,
+            btc: number
+          }) => {
+            return (
+              <ResultRow
+                key={item.id}
+                loading={loading}
+                provider={item?.provider}
+                btc={item?.btc}
+              />
+            )
+          }
+          )
+        }
       </div>
     </main>
   )
--- src/components/ResultRow.tsx
+++ src/components/ResultRow.tsx
@@ -1,16 +1,20 @@
 
 type ResultRowProps = {
     loading?: boolean
+    provider?: string
+    btc?: number
 }
 
 const ResultRow: React.FC<ResultRowProps> = ({
-    loading = true
+    loading = true,
+    provider,
+    btc
 }) => {
     if (loading) {
         return (
             <div className="border rounded-lg border-white/20 p-4
             bg-gradient-to-r skeleton from-indigo-400/30 to-purple-800/10 my-2
-            h-28">
+            h-[120px]">
             </div>
         )
     }
@@ -20,15 +24,15 @@ const ResultRow: React.FC<ResultRowProps> = ({
     ">
             <div className="flex gap-4">
                 <div>logo</div>
-                <div className="flex-1 ">provider name</div>
+                <div className="flex-1 ">{provider?.toUpperCase()}</div>
                 <div className="flex items-center gap-2">
-                    <span className="text-xl text-purple-200/80">0.001</span>
+                    <span className="text-xl text-purple-200/80">{btc}</span>
                     <span className="text-xl text-purple-200/60">BTC</span>
                 </div>
             </div>
 
-            <div className=" flex justify-center mt-4">
-                <button className=" w-24 h-10 bg-gradient-to-r from-indigo-400 to-purple-800 text-white/90
+            <div className=" flex justify-center mt-4 ">
+                <button className=" shine w-24 h-10 bg-gradient-to-r from-indigo-400 to-purple-800 text-white/90
                         rounded-md px-4 py-2 font-bold
                         hover:from-indigo-500 hover:to-purple-900
                         focus:outline-none focus:ring-2 focus:ring-purple-600
